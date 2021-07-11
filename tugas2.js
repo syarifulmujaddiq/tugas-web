6ce3331f56a6626281fa1cdb8eacb55119772ae5
syarifulmujaddiq <86391272+syarifulmujaddiq@users.noreply.github.com>
2021-07-11 14:07:19 +0800

    Add files via upload

---
+++ tugas2.js
@@ -0,0 +1,41 @@
+const http = require('http');
+
+const server = http.createServer((request, response) =>{
+    console.log(request.url);
+    console.log(request.headers);
+    console.log(request.headers['accept-language']);
+    
+    
+    if (request.url ==='/'){
+        response.statusCode = 200;
+        response.setHeader('Content-Type', 'text/html');
+        response.write(`Bahasa = ${request.headers['accept-language']}<br><br>`);
+        //response.write(`request : ${request.headers}`);
+        response.write('Menu <br><br>');
+        response.write("<a href='/makanan'>Menu makanan</a><br>");
+        response.write("<a href='/minuman'>Menu minuman</a><br>");
+        response.end();
+    }else if(request.url ==='/makanan' ) {
+        response.statusCode = 200;
+        response.setHeader('Content-Type', 'text/html');
+        response.write('Ini adalah menu makanan');
+        response.end();
+    }else if(request.url ==='/minuman'){
+        response.statusCode = 200;
+        response.setHeader('Content-Type', 'text/html');
+        response.write('Ini adalah menu minuman');
+        response.end();
+    }else{
+        response.statusCode = 404;
+        response.setHeader('Content-Type', 'text/html');
+        response.write('Menu lain tidak tersedia');
+        response.end();
+    }
+
+
+});
+
+server.listen(4002, () =>{
+    console.log(`server listen https://http://129.213.54.196:4002`);
+    
+});
